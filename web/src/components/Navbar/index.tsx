c8b9653ed19fc3ae673e56c2d4d0c7358be20e04
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-08-12 22:21:11 -0300

    web: add ul list items to aside menu

--- web/src/components/Navbar/index.tsx
+++ web/src/components/Navbar/index.tsx
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
-
-import logo from '../../assets/images/icons/logo.svg';
+import { Pets, Spa, GpsFixed } from '@styled-icons/material';
 
 import './styles.css';
+import logo from '../../assets/images/icons/logo.svg';
 import HamburgerMenu from '../HamburgerMenu';
 import Backdrop from '../Ui/Backdrop';
 
+interface MenuItem {
+  icon: string;
+  label: string;
+  url: string;
+}
+
 const Navbar: React.FC = () => {
-  const [hamburgerMenuVisible, setHamburgerMenuVisible] = useState<boolean>();
+  const [hamburgerMenuVisible, setHamburgerMenuVisible] = useState<boolean>(
+    true
+  );
 
   function handleHamburgerClicked() {
     setHamburgerMenuVisible(!hamburgerMenuVisible);
@@ -18,7 +26,7 @@ const Navbar: React.FC = () => {
       <nav id="navbar">
         <div id="navbar-content">
           <div id="navbar-site-info">
-            <img src={logo} alt="Petty logo" id="navbar-logo" />
+            <img src={logo} alt="Petty logo" className="navbar-logo" />
             <span id="title">Petty</span>
           </div>
           <div id="navar-menu">
@@ -36,7 +44,38 @@ const Navbar: React.FC = () => {
             ? 'off'
             : ''
         }
-      ></div>
+      >
+        <header>
+          <img src={logo} alt="Petty logo" className="navbar-logo" />
+          <h2>Items available</h2>
+        </header>
+        <ul>
+          <li>
+            <a className="link" href="/">
+              <div className="list-item">
+                <Pets className="list-icon" />
+                Adotar
+              </div>
+            </a>
+          </li>
+          <li>
+            <a className="link" href="/">
+              <div className="list-item">
+                <Spa className="list-icon" />
+                Procurar cuidados
+              </div>
+            </a>
+          </li>
+          <li>
+            <a className="link" href="/">
+              <div className="list-item">
+                <GpsFixed className="list-icon" />
+                Anunciar desaparecimento
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
 
       <Backdrop
         clicked={handleHamburgerClicked}
