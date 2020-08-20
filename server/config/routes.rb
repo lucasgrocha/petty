1ceea73c26fd5d9e41c74fec982f37bd067f7ce5
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-08-20 18:07:44 -0300

    add api structure to controllers

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :pets
-  resources :contacts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :pets
+      resources :contacts
+    end
+  end
 end
