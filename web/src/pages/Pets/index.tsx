f5c67929c39742173900555a12263399f01dfe77
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-08-24 17:15:07 -0300

    web: add styles to Pets page

--- web/src/pages/Pets/index.tsx
+++ web/src/pages/Pets/index.tsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
+import './styles.css';
+
+import ImageGallery from 'react-image-gallery';
 import { Pet } from '../../components/PetCard';
 import petService from '../../services/petService';
 import { useParams, useNavigate } from 'react-router-dom';
@@ -16,17 +19,70 @@ const Pets: React.FC = () => {
         .then((res) => {
           setPetData(res.data);
         })
-        .catch((_) => {
+        .catch(() => {
           navigate('/adopt');
         });
     }
   }, [id, petData, navigate]);
 
+  if (petData === undefined) {
+    return null;
+  }
+
+  const IMAGES = petData.pictures_url.map((pic, index) => ({
+    original: pic,
+    thumbnail: pic,
+  }));
+
   return (
     <div className="container">
-      <h1>{petData?.id}</h1>
-      <h1>{petData?.pet_name}</h1>
-      <h1>{petData.contacts.whatsapp}</h1>
+      <div id="pets-wrapper">
+        <div id="pets-info-top">
+          <div id="pets-photo-gallery">
+            <ImageGallery
+              items={IMAGES}
+              showFullscreenButton={false}
+              showPlayButton={false}
+              showThumbnails={false}
+              showBullets={petData.pictures_url.length > 1}
+            />
+          </div>
+          <div id="pets-info-infos">
+            <div className="pet-info-prop">
+              <h3>
+                Name: <span>{petData.pet_name}</span>
+              </h3>
+
+              <h3>
+                Age: <span>{petData.age}</span>
+              </h3>
+
+              <h3>
+                Location: <span>{petData.location}</span>
+              </h3>
+            </div>
+          </div>
+        </div>
+        <hr />
+
+        <div id="pets-info-bottom">
+          <div id="pets-info-description">
+            <p>{petData.description}</p>
+          </div>
+        </div>
+
+        <div id="pet-info-buttons">
+          <a
+            target="_blank"
+            href={`https://wa.me/${petData.contacts.whatsapp}`}
+          >
+            Whatsapp
+          </a>
+          <a target="_blank" href={`mailto:${petData.contacts.email}`}>
+            Email
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
