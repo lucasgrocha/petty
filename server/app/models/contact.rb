304aa717c48e7ad219610c0d44a8290b99907057
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-08-25 15:28:40 -0300

    server: add callback to filter whatsapp number

--- server/app/models/contact.rb
+++ server/app/models/contact.rb
@@ -1,3 +1,11 @@
 class Contact < ApplicationRecord
+  before_create :filter_number
+
   has_one :pet
+
+  private
+
+  def filter_number
+    self.whatsapp = self.whatsapp.scan(/\+\d+|\d+/).join('')
+  end
 end
