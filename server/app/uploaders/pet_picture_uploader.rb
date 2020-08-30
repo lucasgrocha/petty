22480209521494243826a6894f8bd5f1dab6f728
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-08-30 19:59:11 -0300

    server: add method to reduce image quality (carrierwave)

--- server/app/uploaders/pet_picture_uploader.rb
+++ server/app/uploaders/pet_picture_uploader.rb
@@ -6,6 +6,7 @@ class PetPictureUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
+  process quality: 30
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -36,7 +37,7 @@ class PetPictureUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
-    %w(jpg jpeg gif png)
+    %w[jpg jpeg gif png]
   end
 
   # Override the filename of the uploaded files:
