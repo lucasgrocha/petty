ba608b47b87d2997a923dc2ffd0f55dce609122b
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-08-31 16:07:58 -0300

    web: change leaflet map provider & set max zoom

--- web/src/components/LeafletMap/index.tsx
+++ web/src/components/LeafletMap/index.tsx
@@ -28,10 +28,16 @@ const LeafletMap: React.FC<Props> = (props) => {
   console.log(props.markerPosition);
 
   return (
-    <Map center={initialPosition} zoom={17} id="map" onClick={props.mapClicked}>
+    <Map
+      center={initialPosition}
+      zoom={13}
+      maxZoom={17}
+      id="map"
+      onClick={props.mapClicked}
+    >
       <TileLayer
-        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+        url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
       />
 
       <Marker
