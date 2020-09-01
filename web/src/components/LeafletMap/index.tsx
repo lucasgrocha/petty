e244791abbfc05597df290b282a138eaef1c0e89
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-09-01 17:45:36 -0300

    web: change leaflet provider

--- web/src/components/LeafletMap/index.tsx
+++ web/src/components/LeafletMap/index.tsx
@@ -31,16 +31,10 @@ const LeafletMap: React.FC<Props> = (props) => {
   }, [props.markerPosition, props.mapClicked]);
 
   return (
-    <Map
-      center={initialPosition}
-      zoom={13}
-      maxZoom={17}
-      id="map"
-      onClick={props.mapClicked}
-    >
+    <Map center={initialPosition} zoom={13} id="map" onClick={props.mapClicked}>
       <TileLayer
-        attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
-        url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
       <Marker
