2afc53fe990ffbb6356cf3685947df0a88bc8b42
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-09-05 17:04:24 -0300

    web: add search service

--- web/src/components/Search/index.tsx
+++ web/src/components/Search/index.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { FormEvent, useRef } from 'react';
 
 import './styles.css';
 
 import { SearchAlt2 as SearchIcon } from '@styled-icons/boxicons-regular';
 import DropdownItem from './DropdownItem';
+import searchService from '../../services/searchService';
 
 const Search: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  function handleSubmit(evt: FormEvent) {
+    evt.preventDefault();
+
+    const searchTerm = inputRef.current?.value || '';
+    const filteredNumbers = searchTerm?.match(/\d+/) || [];
+
+    const ageSearch = filteredNumbers[0]?.length === searchTerm?.length;
+
+    const searchType = ageSearch ? 'age' : 'address';
+
+    searchService.search(searchTerm, searchType).then((res) => {
+      console.log(res.data);
+    });
+  }
+
   return (
     <div id="search">
-      <form id="search-box">
-        <input type="text" placeholder="Pesquise por localidade ou idade" />
+      <form id="search-box" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Pesquise por localidade ou idade"
+          ref={inputRef}
+        />
         <button type="submit">
           <SearchIcon />
         </button>
