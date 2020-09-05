a009f2fd40ca30b62f869d89e02bb81d5f80b04a
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-09-05 15:48:33 -0300

    web: add grid display to navbar

--- web/src/components/Search/index.tsx
+++ web/src/components/Search/index.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 import './styles.css';
 
+import { SearchAlt2 as SearchIcon } from '@styled-icons/boxicons-regular';
+
 const Search: React.FC = () => {
   return (
     <form id="search-box">
       <input type="text" />
-      <button type="submit">O</button>
+      <button type="submit">
+        <SearchIcon />
+      </button>
     </form>
   );
 };
