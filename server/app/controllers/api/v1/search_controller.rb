30dc2dde585f78cc9c1d05e94cbff8d690a39e1f
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-09-05 22:07:42 -0300

    server: select useful columns from Pet

--- server/app/controllers/api/v1/search_controller.rb
+++ server/app/controllers/api/v1/search_controller.rb
@@ -2,8 +2,13 @@ class Api::V1::SearchController < ApplicationController
   before_action :get_params
 
   def index
-    @pets = Pet.where("#{@search_type} = ?", @search_term)
-    puts @pets.size
+    @pets = Pet
+            .select(:id,
+                    :age,
+                    :pet_name,
+                    :address,
+                    :pictures,
+                    :status).where("#{@search_type} = ?", @search_term)
   end
 
   private
