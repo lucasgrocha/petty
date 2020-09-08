5cfbbe9ed0646310927e64523ff985b9fac0211a
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-09-07 21:11:58 -0300

    server: improve searching

--- server/app/controllers/api/v1/search_controller.rb
+++ server/app/controllers/api/v1/search_controller.rb
@@ -8,13 +8,19 @@ class Api::V1::SearchController < ApplicationController
                     :pet_name,
                     :address,
                     :pictures,
-                    :status).where("#{@search_type} = ?", @search_term)
+                    :status)
+            .where("#{address_query? ? "lower(#{@search_type}) LIKE " : "#{@search_type} = "} ?",
+                   (address_query? ? "%#{@search_term}%" : @search_term).to_s)
   end
 
   private
 
   def get_params
-    @search_term = params[:search_term]
+    @search_term = params[:search_term].downcase
     @search_type = params[:search_type]
+  end
+
+  def address_query?
+    @search_type == 'address'
   end
 end
